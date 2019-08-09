7565efeba1d17a66075282f57f676455cf439d99
ade <ade_ari_w0@yahoo.com>
2019-08-09 23:01:16 +0700

    dgd

--- klass/kirim.go
+++ klass/kirim.go
@@ -1,6 +1,7 @@
 package klass
 
 import (
+  "fmt"
   "net/http"
   "github.com/labstack/echo/v4"
   _ "github.com/go-sql-driver/mysql"
@@ -17,8 +18,9 @@ func Kirim(c echo.Context) error {
   db := connect()
   defer db.Close()
   
-  db.Exec("insert into ordernya (nama, email, hp, tanggal_pickup, tujuan, drop_off, merk_mobil, tanggal_pickup_show, pesan) values (?,?,?,?,?,?,?,?,?)", c.FormValue("nama"), c.FormValue("email"), c.FormValue("hp"), c.FormValue("tanggal_pickup"), c.FormValue("tujuan"), c.FormValue("dropoff"), c.FormValue("merkmobil"), c.FormValue("tanggal_pickup_show"), c.FormValue("pesan"))
-  
+  a,b := db.Exec("insert into ordernya (nama, email, hp, tanggal_pickup, tujuan, drop_off, merk_mobil, tanggal_pickup_show, pesan) values (?,?,?,?,?,?,?,?,?)", c.FormValue("nama"), c.FormValue("email"), c.FormValue("hp"), c.FormValue("tanggal_pickup"), c.FormValue("tujuan"), c.FormValue("dropoff"), c.FormValue("merkmobil"), c.FormValue("tanggal_pickup_show"), c.FormValue("pesan"))
+  fmt.Println(a)
+  fmt.Println(b)
   return c.JSON(http.StatusOK, msg)
 }
 
