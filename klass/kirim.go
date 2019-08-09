fd95c8e9691e501b07bd6ae6523f1791815434d8
ade <ade_ari_w0@yahoo.com>
2019-08-09 22:17:25 +0700

    ada

---
+++ klass/kirim.go
@@ -0,0 +1,28 @@
+package klass
+
+import (
+  "net/http"
+  "github.com/labstack/echo/v4"
+  _ "github.com/go-sql-driver/mysql"
+)
+
+type Message struct {
+  Msg  string `json:"msg"`
+  Status  bool `json:"status"`
+}
+
+func Kirim(c echo.Context) error {
+  msg := &Message{Msg:"", Status:true}
+  
+  db := connect()
+  defer db.Close()
+  
+  db.Exec("insert into ordernya (nama, email, hp, tanggal_pickup, tujuan, drop_off, merk_mobil, tanggal_pickup_show, pesan) values (?,?,?,?,?,?,?,?,?)", c.FormValue("nama"), c.FormValue("email"), c.FormValue("hp"), c.FormValue("tanggal_pickup"), c.FormValue("tujuan"), c.FormValue("dropoff"), c.FormValue("merkmobil"), c.FormValue("tanggal_pickup_show"), c.FormValue("pesan"))
+  
+  return c.JSON(http.StatusOK, msg)
+}
+
+func Coba(c echo.Context) error {
+  msg := &Message{Msg:"aaaaa", Status:true}
+  return c.JSON(http.StatusOK, msg)
+}
--- rentcar_server.go
+++ rentcar_server.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"net/http"	
-	"github.com/labstack/echo"
+	"net/http"
+    "github.com/labstack/echo/v4"
+    "github.com/labstack/echo/v4/middleware"
 	"html/template"
     "io"
     "rentcar/klass"
@@ -27,14 +28,30 @@ func indexawal(c echo.Context) error {
     return c.Render(http.StatusOK, "index.html", map[string]interface{}{})
 }
 
+
+
 func main() {
   e := echo.New()
+  
+  e.Use(middleware.Logger())
+  e.Use(middleware.Recover())
+   
+  e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+    AllowOrigins: []string{"*"},
+    AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
+    AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+  }))
+  
   e.Static("/", "assets")
   
   renderer := &TemplateRenderer{
     templates: template.Must(template.ParseGlob("html/*.html")),
   }
   e.Renderer = renderer
-  e.GET("/", indexawal)  
+  
+  e.GET("/", indexawal)
+  e.POST("kirim", klass.Kirim)
+  e.POST("coba", klass.Coba)
+  
   e.Logger.Fatal(e.Start(":"+klass.PORT))
 } 
